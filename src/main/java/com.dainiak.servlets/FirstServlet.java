1873b30cadb05107045882b7b527334137bb65e4
GasDa <270309Ab>
2016-05-14 10:44:32 +0300

    from ide

--- src/main/java/com.dainiak.servlets/FirstServlet.java
+++ src/main/java/com.dainiak.servlets/FirstServlet.java
@@ -1,7 +1,25 @@
 package com.dainiak.servlets;
 
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
 /**
  * Created by Alex Dainiak on 14-05-2015.
  */
-public class FirstServlet {
+public class FirstServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(FirstServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to userList");
+
+//        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+        response.sendRedirect("userList.jsp");
+    }
 }
